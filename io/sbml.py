b3f23bb04ba0f50d35ec363fbe5fa21bc3c573a1
Nikolaus Sonnenschein <niko@magenta.iuhb02.iu-bremen.de>
2010-01-11 15:23:42 +0100

    libsbml is crashing under os x. will continue development under Fedora

--- io/sbml.py
+++ io/sbml.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 # encoding: utf-8
 """
 sbml.py
@@ -9,12 +9,22 @@ Copyright (c) 2010 Jacobs University of Bremen. All rights reserved.
 
 import sys
 import os
+from pyMetabolism.metabolism import Metabolism, Reaction, Compound
+try:
+    import libsbml
+except ImportError, msg:
+    print "Please install libsbml to use SBML functionality!"
+    print msg
 
+def sbml2metabolism(path):
+    """Loads a SBML model and returns a Metabolism object."""
+    model = libsbml.readSBML('../test_data/iAF1260.xml').getModel()
+    reactionsList = model.getListOfReactions()
+    return reactionsList
 
-def main():
-    pass
 
 
 if __name__ == '__main__':
-    main()
-
+    tmp = sbml2metabolism('../test_data/iAF1260.xml')
+    print tmp[0]
+    
