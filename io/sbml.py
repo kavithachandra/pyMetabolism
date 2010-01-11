ccda7ea07c1006b84f4ddccb37d33f72204666ad
Nikolaus Sonnenschein <niko@magenta.iuhb02.iu-bremen.de>
2010-01-11 13:42:06 +0100

    Will implement some SBML functionality using libsbml
    Added one test SBML file
    Added a file under io for the routines

---
+++ io/sbml.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+sbml.py
+
+Created by Nikolaus Sonnenschein on 2010-01-11.
+Copyright (c) 2010 Jacobs University of Bremen. All rights reserved.
+"""
+
+import sys
+import os
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
