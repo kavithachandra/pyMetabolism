bceb6d32a7acde5dd1f7157d4de9dd89da70a7da
Moritz Beber <engineer@scorpion.(none)>
2010-03-15 19:24:20 +0100

    made OptionsManager a Singleton
    OptionsManager can now detect the number of cpus automatically.

--- __init__.py
+++ __init__.py
@@ -17,25 +17,86 @@ with transcription data (PDA).
 
 """
 
-
+import os
 import subprocess
-import sys
 
 
 class OptionsManager(object):
     """
     This class unifies some global options, like certain name pre- and suffixes,
-    number of cpus to use for certain parallel tasks, etc.
+    number of cpus to use for certain parallel tasks, etc. This class is modelled
+    as a singleton, i.e., only one instance of this class may exist.
+    @todo: Consider thread safety, though I would defer that to using this class.
     """
+    _singleton = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._singleton:
+            cls._singleton = super(OptionsManager, cls).__new__(*args, **kwargs)
+        return cls._singleton
+
     def __init__(self):
-        self.n_cpus = 1
         self.metb_prefix = "M_"
         self.rxn_prefix = "R_"
         self.rev_rxn_suffix = "_Rev"
-        self._find_cpus()
         self.logger_main = "pyMetabolism"
+        self.n_cpus = self._find_num_cpus()
 
-    def _find_cpus(self):
-        pass
+    def _find_num_cpus(self):
+        """
+        Detects the number of effective CPUs in the system. Adapted from Parallel
+        Python.
+        @author: Vitalii Vanovschi
+        @copyright: (c) 2005-2010. All rights reserved.
+        @contact: http://www.parallelpython.com
+        """
+        num_cpus = None
+        # for Linux, Unix and MacOS
+        if hasattr(os, "sysconf"):
+            if "SC_NPROCESSORS_ONLN" in os.sysconf_names:
+                #Linux and Unix
+                try:
+                    num_cpus = int(os.sysconf("SC_NPROCESSORS_ONLN"))
+                except:
+                    pass
+                else:
+                    if num_cpus > 0:
+                        return num_cpus
+        # Linux, Unix failsafe
+        cmd = ["grep", "-c", "'model name'", "'/proc/cpuinfo'"]
+        p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+        (stdout, stderr) = p.communicate()
+        if p.returncode == 0:
+            try:
+                num_cpus = int(stdout)
+            except:
+                pass
+            else:
+                if num_cpus > 0:
+                    return num_cpus
+        # MacOS X
+        cmd = ["sysctl", "-n", "hw.ncpu"]
+        p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+        (stdout, stderr) = p.communicate()
+        if p.returncode == 0:
+            try:
+                num_cpus = int(stdout)
+            except:
+                pass
+            else:
+                if num_cpus > 0:
+                    return num_cpus
+        #for Windows
+        if "NUMBER_OF_PROCESSORS" in os.environ:
+            try:
+                num_cpus = int(os.environ["NUMBER_OF_PROCESSORS"])
+            except:
+                pass
+            else:
+                if num_cpus > 0:
+                    return num_cpus
+        return 1
 
 # eof
