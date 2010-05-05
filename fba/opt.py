51e6206a851e7ac6fd22ae1a27146c49dcbb2a7c
Nikolaus Sonnenschein <niko@magenta.iuhb02.iu-bremen.de>
2010-05-05 15:43:15 +0200

    fixed a bug in StoichiometricMatrix.reactions

--- fba/opt.py
+++ fba/opt.py
@@ -7,9 +7,9 @@ Created by Nikolaus Sonnenschein on 2010-05-05.
 Copyright (c) 2010 Jacobs University of Bremen. All rights reserved.
 """
 
-from pyMetabolism import OptionsManager
 import numpy
 from openopt import LP
+from pyMetabolism import OptionsManager
 from pyMetabolism.stoichiometry import StoichiometricMatrix
 
 class fba(object):
@@ -21,14 +21,20 @@ class fba(object):
 
 if __name__ == '__main__':
     from pyMetabolism.io.sbml import SBMLParser
-    from pyMetabolism.metabolism import Metabolism
+    from pyMetabolism.metabolism import Metabolism, Reaction
 
     import re
     smallModel = '../test_data/Ec_core_flux1.xml'
     bigModel = '../test_data/iAF1260.xml'
     parser = SBMLParser(smallModel, rprefix='R_', rsuffix='', cprefix='M_', csuffix=re.compile('_.$'))
     metbol = parser.get_metabolic_system()
-    print metbol
-    # s = StoichiometricMatrix()
-    # s.make_new_from_system(metbol)
-    # print help(LP)
+    s = StoichiometricMatrix()
+    s.make_new_from_system(metbol)
+    # for elem in s.reactions:
+    #     print elem.identifier
+    print s.reaction_map[Reaction('Biomass_Ecoli_core_N__w_GAM_', (), (), ())]
+    # print Reaction('Biomass_Ecoli_core_N__w_GAM_')
+    
+    obj = numpy.zeros(s.num_reactions)
+    obj[s.reaction_map[Reaction('Biomass_Ecoli_core_N__w_GAM_', (), (), ())]] = 1.
+    lp = 
