d0af08b418cc2871b4cfced7d94249c189f2d366
Nikolaus Sonnenschein <niko@magenta.iuhb02.iu-bremen.de>
2010-05-06 13:31:04 +0200

    fixed glpk.py

--- fba/opt.py
+++ fba/opt.py
@@ -21,7 +21,7 @@ class fba(object):
 
 if __name__ == '__main__':
     from pyMetabolism.io.sbml import SBMLParser
-    from pyMetabolism.metabolism import Metabolism, Reaction
+    from pyMetabolism.metabolism import Metabolism, Reaction, Compartment
 
     import re
     smallModel = '../test_data/Ec_core_flux1.xml'
@@ -30,11 +30,43 @@ if __name__ == '__main__':
     metbol = parser.get_metabolic_system()
     s = StoichiometricMatrix()
     s.make_new_from_system(metbol)
-    # for elem in s.reactions:
-    #     print elem.identifier
-    print s.reaction_map[Reaction('Biomass_Ecoli_core_N__w_GAM_', (), (), ())]
-    # print Reaction('Biomass_Ecoli_core_N__w_GAM_')
-    
+    bMets =[m for m in s.compounds if m.compartment == Compartment('Extra_organism', ())] 
+    # for m in bMets:
+    #     print m
+    bIndices = list()
+    for m in bMets:
+        bIndices.append(s.compound_map[m])
+    print bIndices
     obj = numpy.zeros(s.num_reactions)
     obj[s.reaction_map[Reaction('Biomass_Ecoli_core_N__w_GAM_', (), (), ())]] = 1.
-    lp = 
+    print len(s.matrix)
+    # for m in bMets:
+    #     s.add_reaction(Reaction('R_'+m.identifier+'_Transp', (m,), (), ()))
+    # A_eq = numpy.delete(numpy.array(s.matrix, dtype=float), bIndices, 0)
+    transpColumn = numpy.zeros((len(s.matrix), len(bIndices)))
+    for i, elem in enumerate(bIndices):
+        transpColumn[elem,i] = 1
+    A_eq = numpy.append(numpy.array(s.matrix, dtype=float), transpColumn, 1)
+    print A_eq
+    print numpy.shape(A_eq)
+    b_eq = numpy.zeros(len(A_eq))
+    print numpy.shape(A_eq)[1]
+    print len(b_eq)
+    print len(numpy.random.randint(-10, -5, numpy.shape(A_eq)[1]))
+    print len(numpy.random.randint(5, 10, numpy.shape(A_eq)[1]))
+    lp = LP(f=obj, Aeq=A_eq, beq=b_eq, lb=numpy.random.randint(-6, -0, numpy.shape(A_eq)[1]), ub=numpy.random.randint(0, 6, numpy.shape(A_eq)[1]))
+    lp.exportToMPS
+    # print help(lp.manage)
+    # lp.solve('cvxopt_lp')
+    # print help(lp.solve)
+    r = lp.solve('glpk', goal='max')
+    # print dir(r)
+    # print r.ff
+    # print r.evals
+    # print r.isFeasible
+    # print r.msg
+    # print r.xf
+    # print r.duals
+    # print r.rf
+    # print r.solverInfo
+    # problem = openopt.LP(f=objective, Aeq=A_eq, beq=b_eq, lb=lb, ub=ub)
